7c4d82b41b144491570efc684cd447195e9bdfec
Umair Ahsan <umairahsan2019@gmail.com>
2025-12-16 02:07:32 +0500

    merged main to main because local changes were made

--- gateway/src/main.ts
+++ gateway/src/main.ts
@@ -7,30 +7,25 @@ import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  
-  app.useGlobalPipes(new ValidationPipe({
-    transform: true,
-    transformOptions: {
-      enableImplicitConversion: true,
-    },
-  }));
-  app.setGlobalPrefix(appConfig.apiPrefix);
-  app.enableCors(
-    {
-      origin: process.env.FRONTEND_URL?.split(',') || ["http://localhost:3001"],
-      credentials: true,
-    }
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
   );
-  
+  app.setGlobalPrefix(appConfig.apiPrefix);
+  app.enableCors({
+    origin: process.env.FRONTEND_URL?.split(',') || ['http://localhost:3001'],
+    credentials: true,
+  });
 
   app.use(cookieParser.default());
 
-
- 
   await app.listen(appConfig.port);
   console.log(`Application is running on: ${await app.getUrl()}`);
-
 }
 
 bootstrap();
-
