c3b3c6ddde2d907116a328f05878753d41c9a5a9
Ahmed-Abbasi-Official <farhad.khi17@gmail.com>
2026-01-13 04:10:47 +0500

    admin-panel-done

--- gateway/src/modules/admin/admin.module.ts
+++ gateway/src/modules/admin/admin.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AdminController } from './controllers/admin.controller';
 import { AdminService } from './services/admin.service';
+import { PrismaService } from 'prisma/prisma.service';
 
 @Module({
   controllers: [AdminController],
-  providers: [AdminService],
+  providers: [AdminService, PrismaService],
   exports: [AdminService],
+  
 })
 export class AdminModule {}
 
--- gateway/src/modules/admin/controllers/admin.controller.ts
+++ gateway/src/modules/admin/controllers/admin.controller.ts
@@ -1,19 +1,46 @@
-import { Controller, Get, Post, Put, Delete, Param, Body, UseGuards } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete, Param, Body, UseGuards, Res, Req } from '@nestjs/common';
 import { AdminService } from '../services/admin.service';
+import { JwtAuthGuard } from 'src/modules/auth/jwt-auth.guard';
 
+ @UseGuards(JwtAuthGuard)
 @Controller('admin')
 export class AdminController {
   constructor(private adminService: AdminService) {}
 
   @Get('users')
-  async getAllUsers() {
-    return this.adminService.getAllUsers();
+  async getAllUsers(@Res()res:Response) {
+    return this.adminService.getAllUsers(res);
   }
 
-  @Put('users/:id/role')
-  async updateUserRole(@Param('id') id: string, @Body() roleDto: any) {
-    return this.adminService.updateUserRole(id, roleDto);
+  @Get('uploads')
+  async getAllUploads(@Res()res:Response) {
+    // Implement logic to get all uploads
+    return this.adminService.getAllUploads(res);
   }
+
+  @Get('subscriptions')
+  async getAllSubscriptions(@Res()res:Response) {
+    // Implement logic to get all subscriptions
+    return this.adminService.getAllSubscriptions(res);
+  }
+
+  @Get('sessions')
+  async getAllSessions(@Res()res:Response) {
+    // Implement logic to get all sessions
+    return this.adminService.getUserChatStats(res);
+  }
+
+    @Get('/dashboard')
+    async getPlatformStats(@Res() res: Response , @Req() req:any) {
+      // Accept only year from query params
+      const y = req.query.year ? parseInt(req.query.year) : undefined;
+      return this.adminService.getDashboardStats(res, y);
+    }
+
+  // @Put('users/:id/role')
+  // async updateUserRole(@Param('id') id: string, @Body() roleDto: any) {
+  //   return this.adminService.updateUserRole(id, roleDto);
+  // }
 }
 
 
